1c0b669a356e158ec07311e7b536f13244dcff7b
Admin <Admin@Admin-PC>
2021-06-24 01:57:13 +0200

    Initial entities for Vote Application

---
+++ VoteApplication/src/main/java/com/VoteApplication/domain/VoteId.java
@@ -0,0 +1,33 @@
+package com.VoteApplication.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.ManyToOne;
+
+@Embeddable
+public class VoteId implements Serializable{
+	
+	
+	private static final long serialVersionUID = 1874873311961727024L;
+	private User user;
+	private Feature feature;
+	
+	@ManyToOne
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	@ManyToOne
+	public Feature getFeature() {
+		return feature;
+	}
+	public void setFeature(Feature feature) {
+		this.feature = feature;
+	}
+	
+	
+
+}
